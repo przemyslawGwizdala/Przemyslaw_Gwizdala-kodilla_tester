53a909e61dcbc738177479b4a69b91306f4352c7
Przemyslaw Gwizdala <przemyslaw_gwizdala@wp.pl>
2023-12-18 12:22:43 +0100

    HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        String MojeImie = "Przemyslaw";
+        int MojWiek = 25;
+        char LiteraNazwiska = 'G';
+
+        System.out.println("Hello World!");
+        System.out.println(MojeImie);
+        System.out.println(MojWiek);
+        System.out.println(LiteraNazwiska);
+    }
+}
---
+++ kodilla-intro/src/test/java/FirstClass.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String MojeImie = "Przemyslaw";
+        int MojWiek = 25;
+        char LiteraNazwiska = 'G';
+        System.out.println("Hello World!");
+        System.out.println(MojeImie);
+        System.out.println(MojWiek);
+        System.out.println(LiteraNazwiska);
+    }
+}
