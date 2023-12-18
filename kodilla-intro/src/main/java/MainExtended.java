49f970ec131cee9647524d7ba47d42206ffd55ed
Przemyslaw Gwizdala <przemyslaw_gwizdala@wp.pl>
2023-12-18 17:30:45 +0100

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,22 @@
+public class MainExtended {
+    public static void main(String[] args) {
+
+        String myPhone = "Motorola";
+        int myFavoriteNumber = 4;
+        double priceForPotato = 0.3;
+        char myAverageGrades = 'B';
+        boolean hardWorkIsWorthIt = true;
+        long porscheCosts = 663000;
+
+        System.out.println(myPhone);
+        System.out.println(myFavoriteNumber);
+        System.out.println(priceForPotato);
+        System.out.println(myAverageGrades);
+        System.out.println(hardWorkIsWorthIt);
+        System.out.println(porscheCosts);
+
+
+
+
+    }
+}
