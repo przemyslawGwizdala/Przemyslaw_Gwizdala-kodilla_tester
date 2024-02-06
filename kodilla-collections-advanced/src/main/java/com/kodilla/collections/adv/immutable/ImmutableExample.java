1ceb312293eebc915a659d76610f714203974cb6
Przemyslaw Gwizdala <przemyslaw.gwizdala@wp.pl>
2024-02-06 11:19:31 +0100

    .

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/ImmutableExample.java
@@ -0,0 +1,7 @@
+package com.kodilla.collections.adv.immutable;
+
+public class ImmutableExample {
+    public static void main(String[] args) {
+        BookRecord bookRecord = new BookRecord ("test", "testowa pozycja", 2022);
+    }
+}
